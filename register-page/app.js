0e227eb8d1b38239d817906e75d8a5dbe7cc7794
Andrew Scancari <andrew.scancari@compasso.com.br>
2019-07-29 15:50:05 -0300

    Revert "Inclusão de exemplo de Angular Material"
    This reverts commit e2886ce3710dcb3f8cbe1920aaf21e145bc2ba06.

--- register-page/app.js
+++ register-page/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'ngMaterial', 'ngMessages']);
+var app = angular.module('app', ['ngRoute']);
 
 app.controller('MainController', function($scope, $route, $routeParams, $location) {
     $scope.$route = $route;
