2a15a3afaffffb4d162e69f4b9adc3ad4e0dca42
Andrew Scancari <andrew.scancari@compasso.com.br>
2019-08-06 13:58:20 -0300

    Inclusão de exemplo de request, com busca por CEP. Usando loading, promise, angular factory e alertifyjs

--- register-page/bootstrap/app.js
+++ register-page/bootstrap/app.js
@@ -1,13 +1,101 @@
 var app = angular.module('app', ['ngRoute']);
 
-app.controller('MainController', function($scope, $route, $routeParams, $location) {
+app.factory("CustomUtils", ($q, $http) => {
+	var service = {};
+
+	service.custom_get = (url, params, do_not_show_loader) => {
+      if(!do_not_show_loader) service.showLoader(true);
+      
+      let promise = $http({
+        method 	     : 'GET',
+        url 	       : url,
+        params       : params
+      }).then(responseSuccess => {
+        service.showLoader(false);
+
+        return responseSuccess;
+      }).catch(responseError => {
+        service.showLoader(false);
+
+        console.error(responseError);
+
+        return $q.reject(responseError);
+      });
+
+      return promise;
+		}
+		
+	service.custom_post = (url, data, do_not_show_loeader) => {
+    
+    if(!do_not_show_loeader) service.showLoading(true);
+    
+		let promise = $http({
+			method 	     : 'POST',
+			url 	       : url,
+			data         : data
+		}).then(responseSuccess => {
+			service.showLoader(false);
+
+			return responseSuccess;
+		}, responseError => {
+			service.showLoader(false);
+			console.error(responseError.statusText);
+		});
+
+		return promise;
+  }
+  
+  service.getAddressByCPF = cep => {
+    if(!cep) {
+      alertify.warning('Ops! CEP em branco.');
+      return $q.reject();
+    }
+    
+    cep = cep.replace(/\D/g,'');
+    if(cep.length < 8) {
+      alertify.warning('Ops! CEP incorreto.');
+      return $q.reject();
+    }
+    
+    let promise;
+
+    promise = service.custom_get('//viacep.com.br/ws/' + cep + '/json/').then(success => {
+      if(!success.data.erro) {
+        alertify.success('Endereço carregado com sucesso!');
+        return success;
+      } else {
+        alertify.warning('Ops! Endereço não encontrado.');
+        return $q.reject(success);
+      }
+    }, function myError(success) {
+      alertify.error('Erro ao executar requisição');
+      return $q.reject(success);
+    });
+
+    return promise;
+  }
+
+	service.showLoader = show => {
+    if(show) {
+      $('input').prop('disabled', true);
+      $('#screenLoader').fadeIn(200);
+    } else {
+      $('#screenLoader').fadeOut(200);
+      $('input').prop('disabled', false);
+    }
+	}
+
+	return service;
+});
+
+app.controller('MainController', ($scope, $route, $routeParams, $location, CustomUtils) => {
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
 
     $scope.userdata = {};
 
-    $scope.mockUserdata = function() {
+    $scope.mockUserdata = () => {
       $scope.userdata = {
         "cpf": "33470616833", 
         "firstname": "Andrew", 
@@ -22,7 +110,19 @@ app.controller('MainController', function($scope, $route, $routeParams, $locatio
       }
     }
 
-    $scope.clearUserdata = function() {
+    $scope.fillAddressByCep = () => {
+      CustomUtils.getAddressByCPF($scope.userdata.cep).then( success => {
+          $scope.userdata.street = success.data.logradouro;
+          $scope.userdata.district = success.data.bairro;
+          $scope.userdata.city = success.data.localidade;
+          $scope.userdata.state = success.data.uf;
+      }, error => {
+        console.log(arguments);
+        console.error('fillAddressByCep', error);
+      });
+    }
+
+    $scope.clearUserdata = () => {
       $scope.userdata = {};
     }
 });
@@ -33,7 +133,7 @@ app.controller('LoginController', function ($scope, $routeParams) {
 
 app.controller('RegisterController', function ($scope, $routeParams) {
     $scope.params = $routeParams;
-
+    
     $(document).ready(function(){
       $('.cpf').mask('000.000.000-00', {reverse: true});
       $('.cep').mask('00000-000');
